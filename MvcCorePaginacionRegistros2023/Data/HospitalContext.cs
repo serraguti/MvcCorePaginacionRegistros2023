fc7b1234b94f419879023fcc29b7d1ee7d3e50e9
serraguti <pacoserranox@gmail.com>
2023-03-13 14:19:40 +0100

    Add project files.

---
+++ MvcCorePaginacionRegistros2023/Data/HospitalContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros2023.Models;
+
+namespace MvcCorePaginacionRegistros2023.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext
+            (DbContextOptions<HospitalContext> options)
+            : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
---
+++ MvcCorePaginacionRegistros2023/Repositories/RepositoryHospital.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros2023.Data;
+using MvcCorePaginacionRegistros2023.Models;
+
+namespace MvcCorePaginacionRegistros2023.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        #region DEPARTAMENTOS
+
+        public async Task<List<Departamento>> GetDepartamentos()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento> FindDepartamento(int iddepartamento)
+        {
+            Departamento departamento =
+                await this.context.Departamentos
+                .FirstOrDefaultAsync(x => x.IdDepartamento == iddepartamento);
+            return departamento;
+        }
+
+        #endregion
+
+        #region EMPLEADOS
+        public async Task<List<Empleado>> GetEmpleados()
+        {
+            return await this.context.Empleados.ToListAsync();
+        }
+
+        public async Task<Empleado> FindEmpleado(int idempleado)
+        {
+            Empleado empleado =
+                await this.context.Empleados
+                .FirstOrDefaultAsync(x => x.IdEmpleado == idempleado);
+            return empleado;
+        }
+        #endregion
+    }
+}
